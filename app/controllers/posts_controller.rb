5e89bdf4811272ca0142980ba891879da2bd9515
Artur Moczulski <artur.moczulski@gmail.com>
2013-12-16 18:54:17 -0800

    covered tutorial till section 5.10

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+  
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
+
+end
